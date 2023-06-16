639c6df12689e1e7436f2944b44390be1702b02a
Frantxu1971 <106550711+Frantxu1971@users.noreply.github.com>
2023-06-16 16:54:26 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        Angulo += 10
-        if (Angulo > 270) {
-            Angulo = 270
+        Angulo += 5
+        if (Angulo > 225) {
+            Angulo = 225
         }
     } else if (receivedNumber == 2) {
-        Angulo += -10
-        if (Angulo < 90) {
-            Angulo = 90
+        Angulo += -5
+        if (Angulo < 135) {
+            Angulo = 135
         }
     } else if (receivedNumber == 3) {
         Velocidad += 10
@@ -20,24 +20,9 @@ radio.onReceivedNumber(function (receivedNumber) {
             Velocidad = 0
         }
     } else if (receivedNumber == 5) {
-        Angulo = 90
+        Angulo = 180
         Velocidad = 0
     }
-})
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(3)
-})
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendNumber(2)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(5)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(4)
 })
 let Angulo = 0
 let Velocidad = 0
